170700c8ddfc4cbb1ca2f126cb840ccb4ebc9c26
tommypic <tommasopiccoli7575@gmail.com>
2023-12-05 23:39:21 +0100

    provaa

--- slide5/Poligono.java
+++ slide5/Poligono.java
@@ -1,21 +1,22 @@
 public class Poligono {
-    private PuntoND vertici[];
-    public Poligono(PuntoND vertici[]) {
-        this.vertici = new PuntoND[vertici.length]; // creo un array di puntatori e allego spazio per le righe
-        for (int j = 0; j < vertici.length; j++) {
-            this.vertici[j] = vertici[j];
-        }
-    }
+  private PuntoND vertici[];
 
-    public double getperimetro() {
-        double perimetro = 0.0;
-        for (int i = 0; i < this.vertici.length; i++) {
-            PuntoND a = this.vertici[i];
-            PuntoND b = this.vertici(i + 1) % vertici.length]; // facendo il resto quando arrivo all'ultimo vertice m
-            perimetro += b.DistanzaDa(a);
-    
+  public Poligono(PuntoND vertici[]) {
+    this.vertici = new PuntoND[vertici.length]; // creo un array di puntatori e allego spazio per le righe
+    for (int j = 0; j < vertici.length; j++) {
+      this.vertici[j] = vertici[j];
     }
-        return perimetro;
+  }
+
+  public double getperimetro() {
+    double perimetro = 0.0;
+    for (int i = 0; i < this.vertici.length; i++) {
+      PuntoND a = this.vertici[i];
+      PuntoND b = this.vertici[(i + 1) % vertici.length]; // facendo il resto quando arrivo all'ultimo vertice m
+      perimetro += b.DistanzaDa(a);
+
     }
+    return perimetro;
+  }
 
 }
